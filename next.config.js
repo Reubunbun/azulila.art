70d393b0c284382d56d8a867c2cebcb8c3a95eb5
Reubunbun <reuben.luke.p@gmail.com>
2022-04-20 22:32:47 +0100

    ignore ts build errors

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     domains: ['tania-portfolio.s3.eu-west-1.amazonaws.com']
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
