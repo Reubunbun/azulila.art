121ba7f096f2af79eeef88076fd0276152c699b0
Reubunbun <reuben.luke.p@gmail.com>
2023-10-14 14:52:11 +0100

    fix ui when out of stock

--- components/ProductModal/ProductModal.tsx
+++ components/ProductModal/ProductModal.tsx
@@ -16,7 +16,10 @@ const MAX_ALLOWED_QUANTITY = Infinity;
 
 const ProductModal: FC<Props> = ({ productGroup, close }) => {
   const [selectedProductId, setSelectedProductId] = useState<number>(
-    productGroup.products[0].productId,
+    (
+      productGroup.products.filter(p => p.stock > 0)[0] ||
+      productGroup.products[0]
+    ).productId,
   );
   const [quantity, setQuantity] = useState<number>(1);
   const warningDiv = useRef<HTMLDivElement>(null);
@@ -71,63 +74,68 @@ const ProductModal: FC<Props> = ({ productGroup, close }) => {
               </pre>
             </div>
             <div>
-              {productGroup.products.length > 1 &&
-                <div className={styles.containerSelect}>
-                  <p>Select Option:</p>
-                  <select
-                    className={sharedStyles.select}
-                    onChange={e => setSelectedProductId(Number(e.target.value))}
-                  >
-                    {productGroup.products.map(product =>
-                      <option
-                        key={product.productId}
-                        value={product.productId}
-                        disabled={product.stock <= 0}
+              {productGroup.products.filter(p => p.stock > 0).length > 1
+                ? <>
+                    <div className={styles.containerSelect}>
+                      <p>Select Option:</p>
+                      <select
+                        className={sharedStyles.select}
+                        onChange={e => setSelectedProductId(Number(e.target.value))}
                       >
-                        {`${product.name} - ${product.price}$`}
-                      </option>
-                    )}
-                  </select>
-                </div>
-              }
-              <div className={styles.containerWarnings} ref={warningDiv}>
-                <p style={{ display: quantity === 0 ? undefined : 'none' }}>
-                  You must purchase at least one
-                </p>
-                <p style={{ display: quantity > selectedProduct.stock ? undefined : 'none' }}>
-                  Sorry, there is only {selectedProduct.stock} left in stock, please select a lower number
-                </p>
-              </div>
-              <div className={styles.containerQuantityAndPrice}>
-                <div className={styles.containerQuantity}>
-                  <p>Quantity:</p>
-                  <input
-                    className={sharedStyles.numericalInput}
-                    type='text'
-                    max={Math.min(MAX_ALLOWED_QUANTITY, selectedProduct.stock)}
-                    value={quantity}
-                    onChange={e => {
-                      if (/[^0-9]/.test(e.target.value)) {
-                        return;
-                      }
+                        {productGroup.products.map(product =>
+                          <option
+                            key={product.productId}
+                            value={product.productId}
+                            disabled={product.stock <= 0}
+                          >
+                            {`${product.name} - ${product.price}$`}
+                          </option>
+                        )}
+                      </select>
+                    </div>
+                    <div className={styles.containerWarnings} ref={warningDiv}>
+                      <p style={{ display: quantity === 0 ? undefined : 'none' }}>
+                        You must purchase at least one
+                      </p>
+                      <p style={{ display: quantity > selectedProduct.stock ? undefined : 'none' }}>
+                        Sorry, there is only {selectedProduct.stock} left in stock, please select a lower number
+                      </p>
+                    </div>
+                    <div className={styles.containerQuantityAndPrice}>
+                      <div className={styles.containerQuantity}>
+                        <p>Quantity:</p>
+                        <input
+                          className={sharedStyles.numericalInput}
+                          type='text'
+                          max={Math.min(MAX_ALLOWED_QUANTITY, selectedProduct.stock)}
+                          value={quantity}
+                          onChange={e => {
+                            if (/[^0-9]/.test(e.target.value)) {
+                              return;
+                            }
 
-                      setQuantity(Number(e.target.value));
-                    }}
-                  />
-                </div>
-                <div>
-                  <p className={styles.price}>
-                    {selectedProduct.price * quantity}$
-                  </p>
-                </div>
-              </div>
+                            setQuantity(Number(e.target.value));
+                          }}
+                        />
+                      </div>
+                      <div>
+                        <p className={styles.price}>
+                          {selectedProduct.price * quantity}$
+                        </p>
+                      </div>
+                    </div>
+                  </>
+                : <p>Sorry, this item is no longer in stock</p>
+              }
             </div>
             <div>
-              <div className={styles.containerBasketButton}>
-                <button className='commission-btn' onClick={addProductToBasket}>
-                  Add To Basket
-                </button>
-              </div>
+              {productGroup.products.filter(p => p.stock > 0).length > 1 &&
+                <div className={styles.containerBasketButton}>
+                  <button className='commission-btn' onClick={addProductToBasket}>
+                    Add To Basket
+                  </button>
+                </div>
+              }
             </div>
           </div>
           <div className={styles.containerImage}>
